a395d65abe49fbab17904d407e984c9adbf953ac
Akbar Bagus Wicaksana <110794387+Akbarbagusw@users.noreply.github.com>
2026-03-02 17:47:19 +0700

    Modifikasi

--- jobsheet2/MahasiswaMain2.java
+++ jobsheet2/MahasiswaMain2.java
@@ -1,8 +1,8 @@
 public class MahasiswaMain2{
     public static void main(String[] args) {
         Mahasiswa2 mhs1 = new Mahasiswa2();
-        mhs1.nama = "Akbar Bagus Wicaksana";
-        mhs1.nim = "254107020067";
+        mhs1.nama = "Neymar";
+        mhs1.nim = "25410702007";
         mhs1.kelas = "TI-1H";
         mhs1.ipk = 3.7;
 
@@ -14,5 +14,8 @@ public class MahasiswaMain2{
         Mahasiswa2 mhs2 = new Mahasiswa2("Messi", "254107020068", 3.2, "TI-1C");
         mhs2.updateIPK(3.8);
         mhs2.tampilkanInformasi();
+
+        Mahasiswa2 mhsAkbar = new Mahasiswa2("Akbar Bagus Wicaksana", "254107020067", 3.7, "TI-1H");
+        mhsAkbar.tampilkanInformasi();
     }
 }
